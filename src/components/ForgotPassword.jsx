795a899aee918b662b2b10a89a3ac4ee87861996
Tanmayjain0906 <tanmayjain983@gmail.com>
2023-12-23 22:58:22 +0530

    podcast

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -3,17 +3,24 @@ import { toast } from 'react-toastify';
 import { auth } from '../firebase';
 import {sendPasswordResetEmail } from "firebase/auth";
 import { useNavigate } from 'react-router-dom';
+import loginOrSingupContext from '../context/checking/loginOrSingupContext';
+import { useContext } from 'react';
 
 function ForgotPassword() {
     const [email, setEmail] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const {setIsForgot} = useContext(loginOrSingupContext);
 
     const navigate = useNavigate();
 
     const handleForm = async(e) => {
         e.preventDefault();
+        setLoading(true);
         if(!email)
         {
             toast.error("Please enter your email");
+            setLoading(false);
         }
         else
         {
@@ -22,11 +29,14 @@ function ForgotPassword() {
             await sendPasswordResetEmail(auth, email);
             toast.success("Link sent to your gmail account");
             setEmail("");
+            setLoading(false);
+            setIsForgot(false);
             navigate("/");
            }
            catch(error)
            {
             toast.error(error.message);
+            setLoading(false);
            }
         }
     }
@@ -35,7 +45,7 @@ function ForgotPassword() {
         <h1>Forgot Password</h1>
         <form onSubmit={handleForm}>
             <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-            <button type="submit">Submit</button>
+            <button type="submit">{loading ? "Please Wait...": "Submit"}</button>
         </form>
 
 
